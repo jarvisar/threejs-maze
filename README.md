499ce7aaef5f749081a9ac9e9045c4fa41666d93
Adam Jarvis <55147029+jarvisar@users.noreply.github.com>
2023-04-19 16:19:29 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# threejs-maze
-Procedurally Generated Maze
+# Solar System & Flight Simulator
+
+My first real [Three.js](https://threejs.org/) project.
+
+Visit the [GitHub Pages site](https://jarvisar.github.io/solar-system/) to access the latest deployment.
+
+This model includes 1 sun, 8 planets, 9 moons, 4 dwarf planets, and 2 asteroid belts.
+
+###### Note: Performance can be slow while loading all the textures. Disable moons and dwarf planets in Controls to improve performance.
+
+### Explore Mode
+
+Click on individual planets or moons to move the camera and focus on that object.
+
+Use either the mouse or WASD to move the camera. Use the scroll wheel or R/F to zoom in and out.
+
+Press the escape key at any time to enter free view. Hold right click and drag the mouse to pan.
+
+Press the space key to start and stop rotation.
+
+Click on the UFO to switch to flight mode and fly around the solar system. 
+
+### Flight Mode
+
+Use W/S to increase or decrease throttle. Use A/D to roll. Use the mouse to control yaw and pitch. Use space to stop and set the speed to 0. 
+
+Press the escape key at any time to exit flight mode and switch the focus to the nearest planet.
+
+<p align="left">
+  <img src="https://github.com/jarvisar/solar-system/blob/main/public/icon/android-chrome-192x192.png" alt="Three.js" title"Three.js">
+</p>
+
+## Screenshots
+
+<p align="center">
+  
+ <kbd>
+  <img src="https://jarvisar.github.io/assets/img/portfolio/portfolio-11-3.png" alt="All Orbits" title"All Orbits" width="750">
+ </kbd>
+<br>
+All Orbits
+<br>
+<br>
+ 
+ <kbd>
+  <img src="https://jarvisar.github.io/assets/img/portfolio/portfolio-11-2.png" alt="Earth & Moon" title"Earth & Moon" width="750">
+ </kbd>
+ 
+<br>
+Earth & Moon
+<br>
+<br>
+
+<kbd>
+  <img src="https://jarvisar.github.io/assets/img/portfolio/portfolio-11-4.jpg" alt="Saturn, Enceladus, & Iapetus" title"Saturn, Enceladus, & Iapetus" width="750">
+</kbd>
+
+<br>
+Saturn, Enceladus, & Iapetus
+<br>
+<br>
+
+<kbd>
+ <img src="https://jarvisar.github.io/assets/img/portfolio/portfolio-11-5.jpg" alt="Pluto" title"Pluto" width="750">
+</kbd>
+<br>
+Pluto
+<br>
+<br>
+
+<kbd>
+ <img src="https://jarvisar.github.io/assets/img/portfolio/portfolio-11-6.jpg" alt="Ganymede, Jupiter, & Io" title"Ganymede, Jupiter, & Io" width="750">
+</kbd>
+<br>
+Ganymede, Jupiter, & Io
+</p>
---
+++ main.js
@@ -0,0 +1,299 @@
+import GUI from "https://cdn.skypack.dev/lil-gui@0.18.0";
+import { MathUtils, Clock } from "https://cdn.skypack.dev/three@0.149.0";
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.149.0/examples/jsm/controls/OrbitControls'
+import { PointerLockControls } from 'https://cdn.skypack.dev/three@0.149.0/examples/jsm/controls/PointerLockControls'
+import { DragControls } from 'https://cdn.skypack.dev/three@0.149.0/examples/jsm/controls/DragControls'
+import * as THREE from "https://cdn.skypack.dev/three@0.149.0";
+import  { Perlin, FBM } from "https://cdn.skypack.dev/three-noise@1.1.2";
+import * as CANNON from 'https://cdn.skypack.dev/cannon-es';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.149.0/examples/jsm/loaders/GLTFLoader'
+import { FlyControls } from "./FlyControls.js";
+
+
+// create a scene and camera and renderer and add them to the DOM with threejs and cannon
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const canvas = document.querySelector('.webgl')
+
+
+// create a cannon world
+var world = new CANNON.World();
+world.gravity.set(0, -9.82 * 20, 0);
+world.broadphase = new CANNON.NaiveBroadphase();
+world.solver.iterations = 10;
+
+// create a light and add it to the scene up top and add a shadow to the renderer
+const light = new THREE.DirectionalLight(0xaaaaaa, 0.7);
+light.position.set(0, 10, 10);
+light.castShadow = true;
+light.shadow.mapSize.width = 1024;
+light.shadow.mapSize.height = 1024;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 500;
+scene.add(light);
+
+// Set up the ground plane to cast shadows
+const phongMaterial = new THREE.MeshPhongMaterial()
+const planeGeometry = new THREE.PlaneGeometry(25, 25)
+const planeMesh = new THREE.Mesh(planeGeometry, phongMaterial)
+planeMesh.rotateX(-Math.PI / 2)
+planeMesh.position.y = -5;
+planeMesh.receiveShadow = true
+scene.add(planeMesh)
+const planeShape = new CANNON.Plane()
+const planeBody = new CANNON.Body({ mass: 0 })
+planeBody.addShape(planeShape)
+planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
+planeBody.position.y = -5;
+world.addBody(planeBody)
+
+var mazeWidth = 100;
+var mazeHeight = 100;
+
+// prim's algorithm to generate a maze
+function generateMaze(width, height) {
+    var maze = new Array(width);
+    for (var i = 0; i < width; i++) {
+        maze[i] = new Array(height);
+        for (var j = 0; j < height; j++) {
+            maze[i][j] = 0;
+        }
+    }
+
+    var visited = new Array(width);
+    for (var i = 0; i < width; i++) {
+        visited[i] = new Array(height);
+        for (var j = 0; j < height; j++) {
+            visited[i][j] = false;
+        }
+    }
+
+    var walls = [];
+    var start = [Math.floor(Math.random() * width), Math.floor(Math.random() * height)];
+    visited[start[0]][start[1]] = true;
+    walls.push(start);
+    
+    while (walls.length > 0) {
+        var wall = walls[Math.floor(Math.random() * walls.length)];
+        var x = wall[0];
+        var y = wall[1];
+        var neighbors = [];
+        if (x > 0 && !visited[x - 1][y]) {
+            neighbors.push([x - 1, y]);
+        }
+        if (x < width - 1 && !visited[x + 1][y]) {
+            neighbors.push([x + 1, y]);
+        }
+        if (y > 0 && !visited[x][y - 1]) {
+            neighbors.push([x, y - 1]);
+        }
+        if (y < height - 1 && !visited[x][y + 1]) {
+            neighbors.push([x, y + 1]);
+        }
+        if (neighbors.length > 0) {
+            var neighbor = neighbors[Math.floor(Math.random() * neighbors.length)];
+            visited[neighbor[0]][neighbor[1]] = true;
+            walls.push(neighbor);
+            if (neighbor[0] == x - 1) {
+                maze[x - 1][y] = 1;
+            } else if (neighbor[0] == x + 1) {
+                maze[x][y] = 1;
+            } else if (neighbor[1] == y - 1) {
+                maze[x][y - 1] = 1;
+            } else if (neighbor[1] == y + 1) {
+                maze[x][y] = 1;
+            }
+        } else {
+            walls.splice(walls.indexOf(wall), 1);
+        }
+    }
+
+    return maze;
+}
+
+// generate maze
+var maze = generateMaze(mazeWidth, mazeHeight);
+
+// create a box to represent a wall for each cell in the maze
+for (var i = 0; i < mazeWidth; i++) {
+    for (var j = 0; j < mazeHeight; j++) {
+        if (maze[i][j] == 0) {
+            const wallGeometry = new THREE.BoxGeometry(1, 1, 1);
+            const wallMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+            const wall = new THREE.Mesh(wallGeometry, wallMaterial);
+            wall.position.set(i, 0.5, j);
+            scene.add(wall);
+        }
+    }
+}
+
+// use cannon physics to check for collisions
+var world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+world.broadphase = new CANNON.NaiveBroadphase();
+world.solver.iterations = 10
+
+var playerShape = new CANNON.Sphere(0.4);
+var playerBody = new CANNON.Body({ mass: 0 });
+playerBody.addShape(playerShape);
+playerBody.position.set(50, 0.5, 50);
+world.addBody(playerBody);
+
+// Create a Three.js sphere mesh
+var sphereGeometry = new THREE.SphereGeometry(0.4, 32, 32);
+var sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+// shadow
+sphereMaterial.castShadow = true;
+sphereMaterial.receiveShadow = true;
+var sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+// Set the position of the mesh to match the Cannon.js body
+sphereMesh.position.copy(playerBody.position);
+
+// Add the mesh to the scene
+scene.add(sphereMesh);
+
+// move player using WASD
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+
+function onKeyDown(event) {
+    switch (event.keyCode) {
+        case 38: // up
+        case 87: // w
+            moveForward = true;
+            break;
+        case 37: // left
+        case 65: // a
+            moveLeft = true;
+            break;
+        case 40: // down
+        case 83: // s
+            moveBackward = true;
+            break;
+        case 39: // right
+        case 68: // d
+            moveRight = true;
+            break;
+        case 32: // space  
+            if (canJump === true) velocity.y += 350;
+            canJump = false;
+            break;
+    }
+}
+
+function onKeyUp(event) {
+    switch (event.keyCode) {
+        case 38: // up
+        case 87: // w
+            moveForward = false;
+            break;
+        case 37: // left
+        case 65: // a
+            moveLeft = false;
+            break;
+        case 40: // down
+        case 83: // s
+            moveBackward = false;
+            break;
+        case 39: // right
+        case 68: // d
+            moveRight = false;
+            break;
+    }
+}
+
+document.addEventListener('keydown', onKeyDown, false);
+document.addEventListener('keyup', onKeyUp, false);
+
+// update physics
+function updatePhysics() {
+    // Step the physics world
+    world.step(1 / 60);
+
+    // Copy coordinates from Cannon.js to Three.js
+    sphereMesh.position.copy(playerBody.position);
+    sphereMesh.quaternion.copy(playerBody.quaternion);
+}
+
+// update player movement
+function updatePlayer() {
+    if (moveForward) velocity.z -= 400.0;
+    if (moveBackward) velocity.z += 400.0;
+    if (moveLeft) velocity.x -= 400.0;
+    if (moveRight) velocity.x += 400.0;
+
+    // Update the player's velocity
+    playerBody.velocity.x = velocity.x;
+    playerBody.velocity.z = velocity.z;
+
+    // Reset velocity
+    velocity.x *= 0.9;
+    velocity.z *= 0.9;
+
+    // Update the camera
+    
+}
+
+// update loop
+function update() {
+    var time = performance.now();
+
+    updatePhysics();
+    updatePlayer();
+    controls.update();
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(update);
+
+    prevTime = time;
+}
+
+// orbit controls
+var controls = new OrbitControls(camera, canvas);
+controls.target.set(playerBody.position.x, playerBody.position.y, playerBody.position.z);
+controls.update();
+
+// create a box to represent a wall for each cell in the maze
+for (var i = 0; i < mazeWidth; i++) {
+    for (var j = 0; j < mazeHeight; j++) {
+        if (maze[i][j] == 0) {
+            var wallShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
+            var wallBody = new CANNON.Body({ mass: 0 });
+            wallBody.addShape(wallShape);
+            wallBody.position.set(i, 0.5, j);
+            world.addBody(wallBody);
+        }
+    }
+}
+
+// ambient light
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+update();
+
+// konami code listener. If entered, open ./tetris.html
+var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; 
+var konamiCodePosition = 0;
+document.addEventListener('keydown', function(e) {
+    var key = parseInt(e.keyCode || e.which);
+    if (key == konamiCode[konamiCodePosition]) {
+        konamiCodePosition++;
+        if (konamiCodePosition == konamiCode.length) {
+            window.location = './tetris.html';
+        }
+    } else {
+        konamiCodePosition = 0;
+    }
+});
